5e9ce61ce5700211911175bb0fa0dfe779ce48bb
Waleed Ahmed <waleed.wit@gmail.com>
2022-08-10 16:38:59 +0300

    Initial Commit

---
+++ CryptoSwiftExample/CryptoSwiftExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoSwiftExampleApp.swift
+//  CryptoSwiftExample
+//
+//  Created by Waleed on 10/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoSwiftExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
